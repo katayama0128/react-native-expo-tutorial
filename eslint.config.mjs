92008b2f02dad280700f7dcf6fd520c3940cd1a6
tk145 <196783508+katayama0128@users.noreply.github.com>
2025-07-28 10:40:22 +0900

    First commit

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+
+  {
+    rules: {
+      "react/react-in-jsx-scope": 0,
+      "@typescript-eslint/no-require-imports": 0,
+      semi: [2, "never"],
+      "comma-dangle": [2, "never"],
+    },
+  },
+]);
---
+++ src/screens/app.tsx
@@ -0,0 +1,60 @@
+import { type FC, useState } from "react";
+import { StyleSheet, View, type ImageSourcePropType } from "react-native";
+import { registerRootComponent } from "expo";
+import { StatusBar } from "expo-status-bar";
+import * as ImagePicker from "expo-image-picker";
+
+import { Button } from "@/components/button";
+import { ImageViewer } from "@/components/image-viewer";
+
+const PlaceholderImage =
+  require("@/assets/images/background-image.png") as ImageSourcePropType;
+
+const App: FC = () => {
+  const [selectedImage, setSelectedImage] = useState<null | string>(null);
+
+  const pickImageAsync = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("画像が選択されていません");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="写真を選択" onPress={pickImageAsync} />
+        <Button label="この写真を使用" />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
+});
+
+registerRootComponent(App);
